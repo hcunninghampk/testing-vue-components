3d398c4184206950c455d1da046f164fac48c338
Heather Cunningham <hcunningham@patientkeeper.com>
2021-05-03 13:17:34 -0400

    * temperature.spec.js: Test for Watched property for temperature does not work the way Vue states it should.  Has an open bug: https://github.com/vuejs/vue-test-utils/issues/1419  * Commented out test.  Skipping does not work well with output

--- specs/temperature.spec.js
+++ specs/temperature.spec.js
@@ -1,7 +1,8 @@
 import Temperature from '@/temperature'
-import { mount } from '@vue/test-utils'
+import {mount} from '@vue/test-utils'
 
-describe('Computed Properties Tests', () => {
+describe('Computed and Watched Properties Tests', () => {
+
     test('celsius property', () => {
         //const wrapper = mount(Temperature);
         const {vm} = mount(Temperature);
@@ -25,5 +26,32 @@ describe('Computed Properties Tests', () => {
 
         vm.degrees = 16;
         expect(vm.fahrenheit).toBe(60.8);
-    })
+    });
+
+    //can suffix test with .skip or prefix with x to skip, but then lists output as pending test
+    /*test('temperature watcher method', () => {
+        //set the default data to be 40 degrees celsius
+        const wrapper = mount(Temperature, {propsData: {temp: 40/!*, watch: '40c'*!/}});
+        const {vm} = wrapper;
+        expect(vm.degrees).toBe(40);
+        expect(vm.type).toBe('celsius');
+
+        //setProps() allows you to set the props while the comp is mounted already
+        //Sets Wrapper vm props and forces update.
+        wrapper.setProps({temp: '50f'});//This setProps() doesn't work.  Per Vue docs: "setProps could be called only for
+        // top-level component, mounted by mount or shallowMount".  To change the properties in the Vue instance, you
+        // need to use propsData: "pass a propsData object, which will initialize the Vue instance with passed values."
+        //wrapper.setProps({watch: '50f'});//This doesn't work either.
+
+        /!*wrapper.setProps({
+            watch: {
+                temp: {handler: '50f'}
+            }
+        });*!/  //this doesn't work either
+
+        //wrapper.setData({type: 'fahrenheit', degrees: 50}); //this works, but does not test the watched temperature property
+        console.log("Current degrees: " + vm.degrees + " " + vm.type);
+        expect(vm.degrees).toBe(50);
+        expect(vm.type).toBe('fahrenheit');
+    });*/
 });
