d1c8883c8df6773ff177da0ab88f4cea8db8a0ba
Heather Cunningham <hcunningham@patientkeeper.com>
2021-05-03 13:55:16 -0400

    * temperature.spec.js: Fixed test for Watched property by making test async and awaiting nextTick() before expectations

--- specs/temperature.spec.js
+++ specs/temperature.spec.js
@@ -29,29 +29,36 @@ describe('Computed and Watched Properties Tests', () => {
     });
 
     //can suffix test with .skip or prefix with x to skip, but then lists output as pending test
-    /*test('temperature watcher method', () => {
+    test('temperature watcher method', async () => {
         //set the default data to be 40 degrees celsius
-        const wrapper = mount(Temperature, {propsData: {temp: 40/!*, watch: '40c'*!/}});
+        const wrapper = mount(Temperature, {propsData: {temp: 40/*, watch: '40c'*/}});
         const {vm} = wrapper;
         expect(vm.degrees).toBe(40);
         expect(vm.type).toBe('celsius');
 
         //setProps() allows you to set the props while the comp is mounted already
         //Sets Wrapper vm props and forces update.
-        wrapper.setProps({temp: '50f'});//This setProps() doesn't work.  Per Vue docs: "setProps could be called only for
+        //wrapper.setProps({temp: '50f'});//This setProps() doesn't work.  Per Vue docs: "setProps could be called only for
         // top-level component, mounted by mount or shallowMount".  To change the properties in the Vue instance, you
         // need to use propsData: "pass a propsData object, which will initialize the Vue instance with passed values."
         //wrapper.setProps({watch: '50f'});//This doesn't work either.
 
-        /!*wrapper.setProps({
+        /*wrapper.setProps({
             watch: {
                 temp: {handler: '50f'}
             }
-        });*!/  //this doesn't work either
+        });*/  //this doesn't work either
 
         //wrapper.setData({type: 'fahrenheit', degrees: 50}); //this works, but does not test the watched temperature property
+
+        /*To get this to work, must set the test function as an async fcn.  See signature above.  Then, must use/await
+        nextTick() to force a DOM update to override the watch property 'immediate', which is set to true, which only
+        updates the DOM once.  */
+        wrapper.setProps({temp: '50f'});
+        await wrapper.vm.$nextTick();
+
         console.log("Current degrees: " + vm.degrees + " " + vm.type);
         expect(vm.degrees).toBe(50);
         expect(vm.type).toBe('fahrenheit');
-    });*/
+    });
 });
