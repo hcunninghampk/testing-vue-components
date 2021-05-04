9ba0ee6ab994fc0854ed74877fdebf6e85ea96fa
Heather Cunningham <hcunningham@patientkeeper.com>
2021-05-04 10:17:29 -0400

    added salad.spec.js file

---
+++ specs/salad.spec.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
+
+import SaladBowlComponent from '@/salad-bowl.vue'
+import saladStore from '@/store/salad-store.js'
+
+//Vuex is the state management companion to Vue.js
