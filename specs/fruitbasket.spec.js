f7e1c4921d3b859e6f4f31c1031f30c444acdc4c
Heather Cunningham <hcunningham@patientkeeper.com>
2021-05-04 09:07:31 -0400

    fruitbasket.spec.js:  * Finished 1st test: it('should add fruit to the basket with the DOM' * Ex of Methods for traversing the DOM

--- specs/fruitbasket.spec.js
+++ specs/fruitbasket.spec.js
@@ -7,20 +7,36 @@ import { mount } from '@vue/test-utils'
  The objective is to add fruits to the fruit basket using only the mounted DOM -- no direct calling of any meths --
  since care about the bindings being set up correctly. */
 
-it('should add fruit to the basket with the DOM', async () => {
+it('should add fruit to the basket with the DOM', /*async*/ () => {
     const wrapper = mount(FruitBasket);
 
     //What DOM els do we care about? The Input and the Btn
     /* Wrapper has a find meth, uses CSS selectors to find els. Returns Wrapper of first DOM node or Vue component
     matching selector. BUT, this meth is deprecated, of course.  Use findComponent() instead, whch also rtns Wrapper of
      first matching Vue component.*/
-    //const input = wrapper.find('input');
     const textInput = wrapper.find('input[type="text"]');
-    const addButton = wrapper.find('button');
+    const button = wrapper.find('button');
+    expect(wrapper.findAll('li').length).toBe(0);
 
+    // -------------  Test the Text Input --------------------------
     //Using .element can access the actual HTML el
-    //input.element.value = 'banana'; // This assignment doesn't work.
-    await textInput.setValue('banana');
-    await wrapper.vm.$nextTick();
+    textInput.element.value = 'banana'; // This assignment doesn't work until call .trigger on the el.
+    textInput.trigger('input');
+
+    // --OR-- Make test an async fcn, setValue(), and await nextTick(), whch forces a DOM update.
+    //await textInput.setValue('banana');
+    //await wrapper.vm.$nextTick();
     expect(wrapper.vm.fruit).toBe('banana');
+
+    // -------------  Test the Btn --------------------------
+    //Thi should take the banana out of the text input and put it in the basket on-click
+    button.trigger('click');
+    //So the text input should now be empty
+    expect(wrapper.vm.fruit).toBe('');
+    //The array for the basket should now contain banana
+    expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']));
+    //findAll() returns an array of components matching the CSS selector, but is deperacted; use findAllComponents().
+    wrapper.vm.$nextTick(() => {
+        expect(wrapper.findAll('li').length).toBe(1);
+    });
 });
